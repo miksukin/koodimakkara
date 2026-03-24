50aa61eac9c1bb5d05398c80e367ace25ceae377
miksukin <mikael.reijonen@proton.me>
2026-03-25 01:48:24 +0200

    Fixed streams not showing in www.koodimakkara.fi?

--- src/components/TwitchEmbedding.tsx
+++ src/components/TwitchEmbedding.tsx
@@ -4,16 +4,18 @@ type TwitchEmbedProps = {
     channel: string;
     width?: number | string;
     height?: number | string;
+    parent: string;
 };
 
 const TwitchEmbed: React.FC<TwitchEmbedProps> = ({
     channel,
     width = 502,
     height = 305,
+    parent = ["koodimakkara.fi","www.koodimakkara.fi"],
 }) => {
     return (
         <iframe
-            src={`https://player.twitch.tv/?channel=${channel}&parent=koodimakkara.fi`}
+            src={`https://player.twitch.tv/?channel=${channel}&parent=${parent}`}
             height={height}
             width={width}
             allowFullScreen
