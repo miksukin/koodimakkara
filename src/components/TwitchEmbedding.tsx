115dfd902eee85deb52b2da182b3e6a53710ab92
kingkongclover <anton.reijonen@gmail.com>
2026-03-24 16:36:18 +0200

    Force bussy

---
+++ src/components/TwitchEmbedding.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+type TwitchEmbedProps = {
+    channel: string;
+    width?: number | string;
+    height?: number | string;
+};
+
+const TwitchEmbed: React.FC<TwitchEmbedProps> = ({
+    channel,
+    width = 502,
+    height = 305,
+}) => {
+    return (
+        <iframe
+            src={`https://player.twitch.tv/?channel=${channel}&parent=localhost`}
+            height={height}
+            width={width}
+            allowFullScreen
+        />
+    )
+}
+
+export default TwitchEmbed;
--- src/components/Videopage.tsx
+++ src/components/Videopage.tsx
@@ -1,8 +1,11 @@
+import { Typography } from '@mui/material'
+import TwitchEmbed from './TwitchEmbedding'
 import './Videopage.css'
 
 export const Videopage = () => {
   return (
-    <>
+    <>  
+        <Typography className='shadowia' variant="h1" sx={{ color: "#d67100",fontSize: "clamp(3.5rem, 8vw, 6rem)", marginTop: 5, textShadow: 2}}>Videos</Typography>
         <div className="video-div">
             <div className="video-box">
                 <iframe src="https://www.youtube.com/embed/eEt34nVODtE?si=ZVAwq7zPiO07jb6G"></iframe>
@@ -22,6 +25,22 @@ export const Videopage = () => {
 
             <div className="video-box">
                 <iframe src="https://www.youtube.com/embed/A7wldGXG2XU?si=WvGmAVQafEOSIS9j"></iframe>
+            </div>
+        </div>
+
+        <Typography className='shadow' variant="h1" sx={{ color: "#d67100",fontSize: "clamp(3.5rem, 8vw, 6rem)", marginTop: 5}}>Streamers</Typography>
+
+        <div className='video-div' style={{paddingBottom: 20}}>
+            <div className='video-box'>
+                <TwitchEmbed channel='miksukin'/>
+            </div>
+
+            <div className='video-box'>
+                <TwitchEmbed channel='talktotopsu'/>
+            </div>
+
+            <div className='video-box'>
+                <TwitchEmbed channel='cloverhere'/>
             </div>
         </div>
     </>
